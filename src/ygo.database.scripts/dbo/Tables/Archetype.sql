f35dc9e2b614aecba3237afaf740b512c395fca9
fablecode <fablecode@users.noreply.github.com>
2017-11-12 14:51:58 +0000

    Archetype table updated. Auto Identity removed from id column.

--- src/ygo.database.scripts/dbo/Tables/Archetype.sql
+++ src/ygo.database.scripts/dbo/Tables/Archetype.sql
@@ -1,10 +1,9 @@
 ﻿CREATE TABLE [dbo].[Archetype] (
-    [Id]              BIGINT         IDENTITY (1, 1) NOT NULL,
-    [ArchetypeNumber] NVARCHAR (50)  NOT NULL,
-    [Name]            NVARCHAR (255) NOT NULL,
-    [Alias]           NVARCHAR (255) NOT NULL,
-    [Created]         DATETIME2 (7)  NOT NULL,
-    [Updated]         DATETIME2 (7)  NOT NULL,
+    [Id]      BIGINT         NOT NULL,
+    [Name]    NVARCHAR (255) NOT NULL,
+    [Alias]   NVARCHAR (255) NOT NULL,
+    [Created] DATETIME2 (7)  NOT NULL,
+    [Updated] DATETIME2 (7)  NOT NULL,
     CONSTRAINT [PK_Archetypes] PRIMARY KEY CLUSTERED ([Id] ASC)
 );
 
