5f184697add102ffd3fbcf8af079a90f64973e1b
fablecode <fablecode@users.noreply.github.com>
2018-03-26 22:53:48 +0100

    Removed alias column. Added Url column.

--- src/ygo.database.scripts/dbo/Tables/Archetype.sql
+++ src/ygo.database.scripts/dbo/Tables/Archetype.sql
@@ -1,7 +1,7 @@
 ﻿CREATE TABLE [dbo].[Archetype] (
     [Id]      BIGINT         NOT NULL,
     [Name]    NVARCHAR (255) NOT NULL,
-    [Alias]   NVARCHAR (255) NOT NULL,
+    [Url]     VARCHAR (2083) NOT NULL,
     [Created] DATETIME2 (7)  NOT NULL,
     [Updated] DATETIME2 (7)  NOT NULL,
     CONSTRAINT [PK_Archetypes] PRIMARY KEY CLUSTERED ([Id] ASC)
